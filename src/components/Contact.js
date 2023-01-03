ac2a2a6de47ffc2eee97df30b78cfbfaf4dda555
jainpriyanshe <jainpriya9595@gmail.com>
2023-01-03 18:14:30 +0530

    form update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
+  const [formDetails, setFormDetails] = useState({
+    Name: null,
+    Email: null,
+    Message: null,
+  });
+  const sendForm = (e) => {
+    e.preventDefault();
+
+    var xhr = new XMLHttpRequest();
+    //Url of google sheets script.
+    xhr.open(
+      "POST",
+      "https://script.google.com/macros/s/AKfycbwZ5G7ohDSfwRrBamSTL98zQM0rZbktrk6mtxYI1bDAsP7ym7YX8Xve88prdLAV352w-w/exec"
+    );
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        alert("Your message has been submitted successfully.");
+        e.target.reset();
+        setFormDetails({ Name: null, Email: null, Message: null });
+      }
+    };
+    xhr.onerror = (err) => {
+      console.log("err", err);
+    };
+    var encoded = Object.keys(formDetails)
+      .map(function (k) {
+        return encodeURIComponent(k) + "=" + encodeURIComponent(formDetails[k]);
+      })
+      .join("&");
+    xhr.send(encoded);
+  };
   return (
     <section id="contact_section" className="section mt-6">
       <div className="heading is-flex is-flex-direction-column is-align-items-center pb-6">
@@ -10,7 +42,8 @@ export default function Contact() {
         <h2 className="subtitle has-text-grey is-6 ">Get in touch</h2>
       </div>
       <div className="is-flex is-justify-content-center">
-        <div
+        <form
+          onSubmit={sendForm}
           className="box p-5"
           style={{
             width: "600px",
@@ -20,14 +53,23 @@ export default function Contact() {
           }}
         >
           <div className="field">
-            <label className="label">Name</label>
+            <label className="label" htmlFor="Name">
+              Name
+            </label>
             <div className="control">
               <input
+                id="Name"
                 style={{
                   background: "#f1dbf7",
                   borderRadius: "20px",
                   border: "1px solid #a09a9f",
                 }}
+                onChange={(event) =>
+                  setFormDetails({
+                    ...formDetails,
+                    Name: event.target.value,
+                  })
+                }
                 className="input"
                 type="text"
               />
@@ -35,14 +77,20 @@ export default function Contact() {
           </div>
 
           <div className="field">
-            <label className="label">Email</label>
+            <label className="label" htmlFor="Email">Email</label>
             <div className="control  has-icons-right">
               <input
+              id="Email"
                 style={{
                   background: "#f1dbf7",
                   borderRadius: "20px",
                   border: "1px solid #a09a9f",
-                }}
+                }}  onChange={(event) =>
+                  setFormDetails({
+                    ...formDetails,
+                    Email: event.target.value,
+                  })
+                }
                 className="input"
                 type="email"
               />
@@ -56,16 +104,22 @@ export default function Contact() {
           </div>
 
           <div className="field">
-            <label className="label">Message</label>
+            <label className="label" htmlFor="Message">Message</label>
             <div className="control">
-              <textarea
+              <textarea id="Message"
                 style={{
                   background: "#f1dbf7",
                   borderRadius: "20px",
                   border: "1px solid #a09a9f",
-                }}
+                }}  onChange={(event) =>
+                  setFormDetails({
+                    ...formDetails,
+                    Message: event.target.value,
+                  })
+                }
                 className="textarea"
               ></textarea>
+              
             </div>
           </div>
 
@@ -79,7 +133,7 @@ export default function Contact() {
               </button>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </section>
   );
