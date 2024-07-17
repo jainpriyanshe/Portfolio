d9996dd89d8153e25c21480fef323a5c93cc8970
jainpriyanshe <43369344+jainpriyanshe@users.noreply.github.com>
2024-07-17 20:45:20 +0530

    updated: resume and removed cloudfumes data

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,6 +12,51 @@ export default function Experience() {
       </div>
 
       <div className="is-flex is-justify-content-space-evenly is-flex-wrap-wrap mt-6 ">
+
+        <div className="is-half is-flex is-justify-content-center ">
+          {" "}
+          <div
+            className="card flex-row flex-wrap mt-4"
+            style={{
+              borderRadius: "20px",
+              // boxShadow: "1px 1px 2px black",
+              maxWidth: "400px",
+              boxshadow: "0 2.4rem 4.8rem rgba(0, 0, 0, 0.507)",
+              background:
+                "#f1dbf7",
+              // height: "450px",
+            }}
+          >
+            <div className="card-content border-0">
+              <div
+                className="is-flex is-justify-content-center"
+                style={{ fontSize: "20px" }}
+              >
+                <BsCodeSlash />
+              </div>
+              <h4 className="title is-5 is-flex is-justify-content-center ">
+                Software Test Engineer
+              </h4>
+              <h2 className="subtitle has-text-grey is-6 is-flex is-justify-content-center">
+                Necron Technologies
+              </h2>
+
+              <div className="content">
+                <p className="content is-flex has-text-centered  pb-4">
+                  Necron Technologies provides comprehensive recruitment services and proprietary software solutions designed to streamline the hiring process.                 </p>
+                <h6 className="subtitle is-6  has-text-grey is-flex is-justify-content-center">
+                  Skills
+                </h6>
+                <ul>
+                  <li>Manual Testing</li>
+                  <li>Automation Testing</li>
+                  <li>Selenium with Java</li>
+                  <li>API Testing: PostMan</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="is-half is-flex is-justify-content-center  ">
           {" "}
           <div
@@ -36,72 +81,26 @@ export default function Experience() {
                 <BsHeadphones />
               </div>
               <h4 className="title is-5 is-flex is-justify-content-center ">
-                Web Developer{" "}
+                Software Specialist{" "}
               </h4>
               <h2 className="subtitle has-text-grey is-6 is-flex is-justify-content-center">
-                Necron Technologies{" "}
+                eClinicalWorks{" "}
               </h2>
 
               <div className="content">
                 <p className="content is-flex has-text-centered pb-4">
-                  Necron Technologies is a company that provides multiple services, they mainly focus on web development, mobile app development, and recruitment services for their clients.
-                  
-          
+                  eClinicalWorks is a company that sells electronic medical
+                  record (EMR), practice management and personal health record
+                  software and services to healthcare providers.
                 </p>
                 <h6 className="subtitle is-6  has-text-grey is-flex is-justify-content-center ">
                   Skills
                 </h6>
                 <ul>
-                  <li>React Js</li>
-                  <li>HTML/CSS</li>
-                  <li>JavaScript</li>
-                  <li>Figma</li>
-                <li>Git</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="is-half is-flex is-justify-content-center ">
-          {" "}
-          <div
-            className="card flex-row flex-wrap mt-4"
-            style={{
-              borderRadius: "20px",
-              // boxShadow: "1px 1px 2px black",
-              maxWidth: "400px",
-              boxshadow: "0 2.4rem 4.8rem rgba(0, 0, 0, 0.507)",
-              background:
-                "#f1dbf7",
-              // height: "450px",
-            }}
-          >
-            <div className="card-content border-0">
-              <div
-                className="is-flex is-justify-content-center"
-                style={{ fontSize: "20px" }}
-              >
-                <BsCodeSlash />
-              </div>
-              <h4 className="title is-5 is-flex is-justify-content-center ">
-              Associate Software Developer 
-              </h4>
-              <h2 className="subtitle has-text-grey is-6 is-flex is-justify-content-center">
-                Cloudfumes Technology Software                                                                                                   
-              </h2>
-
-              <div className="content">
-                <p className="content is-flex has-text-centered  pb-4">
-               Cloudfumes Technology Software provides custom application development solutions for large established enterprise companies to a small startup organizations.
-                </p>
-                <h6 className="subtitle is-6  has-text-grey is-flex is-justify-content-center">
-                  Skills
-                </h6>
-                <ul>
-                  <li>HTML, CSS</li>
-                  <li>JavaScript</li>
-                  <li>Vue Js</li>
-                  <li>Figma</li>
+                  <li>MYSQL, MSSQL</li>
+                  <li>Analytical and Problem Solving</li>
+                  <li>Writing/documentation and communication skills.</li>
+                  <li>Troubleshooting hardware and software.</li>
                 </ul>
               </div>
             </div>
